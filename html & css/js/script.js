c418ee33a7884ae9415e9ffd8c2926017c967497
hari <ganduharikrishna553@gmail.com>
2026-06-23 12:15:43 +0530

    first commit

---
+++ html & css/js/script.js
@@ -0,0 +1,86 @@
+// console.log("Hello")
+// var num=10
+// var num1=10
+// console.log(num+num1)
+// var num = 11
+// if (num % 2 == 0 ){
+//     console.log("even")
+// } 
+// else {
+//     console.log("odd")
+
+// }
+// let i=10
+// for (let i=1 ; i<=10; i++){
+//     if (i%2==0){
+//         console.log(i,"is even number ")
+//     }
+//     else if (i%2==1){
+//         console.log(i,"is odd number")
+//     }
+    
+// }
+// function myFun(a, b){
+//     console.log(a - b);
+// }
+// myFun(2,3)
+// myFun(5,2)
+
+
+// const fun=function(a, b){
+//     console.log(a+b)
+
+// }
+// fun(5,6)
+   
+// const funs=(a,b)=>{
+//     return a+b
+// }
+
+// const funs=(a,b) => a+b
+// funs(3,1)
+
+
+// Array
+// let data1=[102,29,93,4,"hello",true]
+// let dat2=[100,29,39,"hello",false]
+// let data3=[14,...data1,45,...dat2]
+// console.log(data3)
+
+
+// let dat=[100,29,39,"hello",false]
+// for (let i=0;i<dat.length;i++){
+//     console.log(dat[i])
+// }
+
+
+// let data=[100,29,39,"hello",false]
+
+// data.push(10)
+// console.log(data)
+// data.pop()
+// console.log(data)
+
+let data = [
+
+    {
+        id :10,
+        name: "Asdg",
+        age:32,
+        techstack:["python","java","c"]
+
+    },
+    {
+        id :11,
+        name:"Asaxs",
+        age:12
+    },
+    {
+        id :13,
+        name: "Asfjg",
+        age:15
+    }
+]
+
+console.log(data)
+console.log(data[0],techstack[2])
